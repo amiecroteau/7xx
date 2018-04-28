5437ccf201fee5204021e4700615e307c59bb0b3
amiecroteau <amiecroteau@gmail.com>
2018-04-28 17:14:52 -0400

    added changes to html/css/js. Added mountains to the picture

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,4 +13,21 @@ for (i = 0; i < 50; i++) {
 	        document.body.appendChild(cln);
     }
 
+
+
+    for (i = 0; i < 3; i++) {
+        let v = Math.floor(Math.random() * (350 - 100)) + 0;
+        let y = Math.floor(Math.random() * (window.innerWidth - 0)) + 100;
+        let itm = document.getElementById("mountain");
+        let cln = itm.cloneNode(true);
+        cln.style.left = (y) + 'px';
+        cln.style.zIndex = -122;
+        document.body.appendChild(cln);
+    }
+
 }
+	
+	
+	
+	
+	
